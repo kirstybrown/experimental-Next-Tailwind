692018103ca4cbc62a4eb3f02536c36bb27dccae
brok <kirsty.brown@elca.ch>
2023-04-07 18:30:16 +0200

    feat: added Paragraph and LargeHeading components wih proptypes

---
+++ src/app/components/ui/LargeHeading.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {VariantProps, cva} from 'class-variance-authority';
+import { HTMLAttributes, forwardRef } from 'react';
+import { cn } from '@/app/lib/utils';
+
+
+const headingVariants = cva("text-black dark:text-white text-center lg:text-left font-extrabold leading-tight tracking-tighter",
+{
+    variants: {
+        size: {
+            default: 'text-4xl md:text-5xl lg:text-6xl',
+            lg: 'text-5xl md:text-6xl lg:text-7xl',
+            sm: 'text-2xl, md:text-3xl lg:text-4xl',
+        },
+        defaultVariants: {
+            size: 'default',
+        }
+    }
+});
+
+export interface LargeHeadingProps extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof headingVariants> {
+}
+
+
+ const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps>(
+    ({className, size, children, ...props}, ref) => {
+    return (
+        <p 
+            ref={ref} 
+            {...props} 
+            className={cn(headingVariants({size, className}))}
+        >
+            {children}
+        </p>
+    );
+});
+
+LargeHeading.displayName = 'Paragraph';
+
+export default LargeHeading;
